20404b09e519d4a72e51162df948927810fe058e
Stanislav K <lunaticman@gmail.com>
2017-10-30 16:26:47 +0700

    first spec

--- lib/ipinfo_io.rb
+++ lib/ipinfo_io.rb
@@ -1,5 +1,9 @@
 require "ipinfo_io/version"
 
 module IpinfoIo
+	def self.call
+		
+	end
+
   # Your code goes here...
 end
--- test/ipinfo_io_test.rb
+++ test/ipinfo_io_test.rb
@@ -4,4 +4,8 @@ class IpinfoIoTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::IpinfoIo::VERSION
   end
+
+  def test_machine_location
+  	assert_equal ({country: "Thailand"}), IpinfoIo.call
+  end
 end
