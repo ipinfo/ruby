4191ee1455338db6e739e81adee7d701521cae24
Stanislav (Stas) Katkov <lunaticman@gmail.com>
2017-11-01 01:32:21 +0700

    IpinfoIo::Adapter object created (#8)
    * IpinfoIo::Adapter object created
    
    * slightly fixes to library requirements

--- lib/ipinfo_io.rb
+++ lib/ipinfo_io.rb
@@ -3,43 +3,20 @@
 require "ipinfo_io/version"
 require 'ipinfo_io/errors'
 require 'ipinfo_io/response'
-require 'faraday'
-require 'json'
+require 'ipinfo_io/adapter'
 
 module IpinfoIo
   RATE_LIMIT_MESSAGE = "To increase your limits, please review our paid plans at https://ipinfo.io/pricing"
-  HOST = 'ipinfo.io'
 
   class << self
     attr_accessor :access_token
 
     def lookup(ip=nil)
-      response = Faraday.get(url_builder(ip)) do |req|
-        default_headers.each_pair do |key, value|
-          req.headers[key] = value
-        end
-      end
+      response = Adapter.new(access_token).get(ip)
 
       raise RateLimitError.new(RATE_LIMIT_MESSAGE) if response.status.eql?(429)
 
-      IpinfoIo::Response.from_faraday(response)
-    end
-
-    private
-
-    def url_builder(ip)
-      if access_token
-        "https://#{HOST}/#{ip}?token=#{access_token}"
-      else
-        "https://#{HOST}/#{ip}"
-      end
-    end
-
-    def default_headers
-        {
-          'User-Agent' => "ruby/#{::IpinfoIo::VERSION}",
-          'Accept' => 'application/json'
-        }
+      Response.from_faraday(response)
     end
   end
 end
