f34fadfa7a6d1fd209d2e4a734cf608914e1d316
Stanislav (Stas) Katkov <lunaticman@gmail.com>
2017-11-01 19:36:43 +0700

    dumb-down adapter (#10)

--- lib/ipinfo_io.rb
+++ lib/ipinfo_io.rb
@@ -4,6 +4,7 @@ require "ipinfo_io/version"
 require 'ipinfo_io/errors'
 require 'ipinfo_io/response'
 require 'ipinfo_io/adapter'
+require 'cgi'
 
 module IpinfoIo
   RATE_LIMIT_MESSAGE = "To increase your limits, please review our paid plans at https://ipinfo.io/pricing"
@@ -12,11 +13,17 @@ module IpinfoIo
     attr_accessor :access_token
 
     def lookup(ip=nil)
-      response = Adapter.new(access_token).get(ip)
+      response = Adapter.new(access_token).get(uri_builder(ip))
 
       raise RateLimitError.new(RATE_LIMIT_MESSAGE) if response.status.eql?(429)
 
       Response.from_faraday(response)
+    end
+
+    private
+
+    def uri_builder(ip)
+      ip ? "/#{CGI::escape(ip)}" : '/'
     end
   end
 end
